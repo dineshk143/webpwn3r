84d4a2e9142960efd582858ce142b40e3918c094
dinesh <59991832+dineshk143@users.noreply.github.com>
2021-12-22 14:52:20 +0530

    Add files via upload

--- scan.py
+++ scan.py
@@ -18,7 +18,7 @@ print ga.green+'''
                                                     
         ##############################################################
         #| "WebPwn3r" Web Applications Security Scanner              #
-        #|  By Ebrahim Hegazy - @Zigoo0                              #
+        #|  Bhgurstguhwgehhvbh- @Zigoo0                              #
         #|  This Version Supports Remote Code/Command Execution, XSS #
         #|  And SQL Injection.                                       #
 	#|  Thanks @lnxg33k, @dia2diab @Aelhemily, @okamalo          #
